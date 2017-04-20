4e723cee0566870cd7a831037b3eefbed0c3c810
Tolga Caner <caner.tolga@gmail.com>
2017-04-20 09:31:44 +0300

    initial commit with a dummy get request

---
+++ LocoMotive/src/main/java/app/LocoMotive.java
@@ -0,0 +1,20 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+/**
+ * Created by tolgacaner on 20/04/2017.
+ */
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+public class LocoMotive {
+    public static void main(String[] args) {
+        SpringApplication.run(LocoMotive.class, args); //NOSONAR
+    }
+}
+
